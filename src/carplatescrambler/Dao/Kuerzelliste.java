fc748324ff6585caaac3bd520a702ae30ca66eb3
Maxim Wilman <maxim.wilman@brockhaus-ag.de>
2017-10-10 16:21:12 +0200

    Kuerzelliste gefixt

--- src/carplatescrambler/Dao/Kuerzelliste.java
+++ src/carplatescrambler/Dao/Kuerzelliste.java
@@ -1,8 +1,12 @@
 package carplatescrambler.Dao;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,10 +17,14 @@ public class Kuerzelliste implements IKuerzelliste {
     public List<String> getFileContent()
     {
         try {
-            File file = new File(getClass().getResource("/carplatescrambler/resources/kuerzelliste.txt").getFile());
-            List<String> Lines = Files.readAllLines(file.toPath());
-
-            return Lines;
+            InputStream stream = getClass().getResourceAsStream("/carplatescrambler/resources/kuerzelliste.txt");
+            List<String> lines = new ArrayList<>();
+            BufferedReader in = new BufferedReader(new InputStreamReader(stream));
+            String line;
+            while ((line = in.readLine()) != null) {
+                lines.add(line);
+            }
+            return lines;
         } catch (IOException ex) {
             Logger.getLogger(Kuerzelliste.class.getName()).log(Level.SEVERE, null, ex);
         }
